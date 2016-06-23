8c21c7b346f3a5ac5d811008d5af15dea3de311d
GraemeB <grae22@gmail.com>
2016-06-23 22:04:11 +0200

    Test project and factory test added.

---
+++ GDVCore_Test/Drivetrain/DrivetrainComponentFactory_Test.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GDVCore.Drivetrain;
+
+namespace GDVCore_Test
+{
+  [TestClass]
+  public class DrivetrainComponentFactory_Test
+  {
+    //=========================================================================
+
+    private class MockComponent : DrivetrainComponent
+    {
+      //-----------------------------------------------------------------------
+
+      public MockComponent( string name )
+      :
+        base( name )
+      {
+
+      }
+
+      //-----------------------------------------------------------------------
+
+      public override double ProcessTorqueAndReturnSpeed(
+        double inputTorque,
+        DrivetrainInputProvider inputProvider )
+      {
+        return 0.0;
+      }
+
+      //-----------------------------------------------------------------------
+    }
+
+    //=========================================================================
+
+    [TestMethod]
+    public void InstantiateObject()
+    {
+      DrivetrainComponent component =
+        DrivetrainComponentFactory.CreateComponent<MockComponent>(
+          "TestComponent" );
+
+      Assert.IsNotNull( component );
+      Assert.AreEqual( "TestComponent", component.Name );
+    }
+
+    //-------------------------------------------------------------------------
+  }
+}
